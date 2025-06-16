885cc2fc634e5b581ffdabcd79fe3ab195dbed3c
Somani <khsomani@deloitte.com>
2025-06-16 23:01:13 +0530

    new navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import { Header } from "@/components/header"
-import { HeroSection } from "@/components/hero-section"
-import { BrandStatement } from "@/components/brand-statement"
-import  ProductGrid  from "@/components/product-grid"
-import { FeatureSection } from "@/components/feature-section"
-import { NewsletterSection } from "@/components/newsletter-section"
-import { Footer } from "@/components/footer"
-import Carousel from "@/components/product-carousel"
+import { Header } from "@/components/header";
+import { HeroSection } from "@/components/hero-section";
+import { BrandStatement } from "@/components/brand-statement";
+import ProductGrid from "@/components/product-grid";
+import { FeatureSection } from "@/components/feature-section";
+import { NewsletterSection } from "@/components/newsletter-section";
+import { Footer } from "@/components/footer";
+import Carousel from "@/components/product-carousel";
+import NavbarDemo from "@/components/resize-navbar";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-white">
+      <NavbarDemo />
       <HeroSection />
       <BrandStatement />
       <Carousel /> {/* productCarousel */}
@@ -18,5 +20,5 @@ export default function HomePage() {
       <NewsletterSection />
       <Footer />
     </div>
-  )
+  );
 }
